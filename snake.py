84ad478486653c0372805c344972df919958fef3
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-10-08 19:01:47 +0100

    rotate

--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ import settings
 class Snake:
     def __init__(self):
         self.head_surface = settings.text_surface("hello")
+        self.rotation = 0
         self.position = (9, 9)
         self.vel = (0, 0)
         radius = int(settings.BLOCK_SIZE/2)
@@ -24,12 +25,14 @@ class Snake:
 
     def change_directions(self, direction):
         if direction is not None:
-            direction_str = ['up', 'down', 'left', 'right']
-            direction_arr = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-            new_vel = direction_arr[direction_str.index(direction)]
+            direction_str = ['up', 'right', 'down', 'left']
+            direction_arr = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+            ind = direction_str.index(direction)
+            new_vel = direction_arr[ind]
             if (np.multiply(np.array(new_vel), np.array(self.vel)) == np.array((0, 0))).all():
                 self.vel = new_vel
+                self.rotation = -90*ind
 
 
     def draw(self, display):
-        display.blit(self.head_surface, settings.grid_to_pos(self.position))
+        display.blit(py.transform.rotate(self.head_surface, self.rotation), settings.grid_to_pos(self.position))
