2b61d8723bfc6d5017b2553cf9fb54aadefd015b
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-10-08 22:10:05 +0100

    langdict

---
+++ words.py
@@ -0,0 +1,12 @@
+import PyDictionary
+from wordfreq import top_n_list
+
+lang_dict = {'Arabic': 'ar', 'Bengali': 'bn', 'Bosnian': 'bs', 'Bulgarian': 'bg', 'Catalan': 'ca', 'Chinese': 'zh', 'Croatian': 'hr', 'Czech': 'cs', 'Danish': 'da', 'Dutch': 'nl', 'English': 'en', 'Finnish': 'fi', 'French': 'fr', 'German': 'de', 'Greek': 'el', 'Hebrew': 'he', 'Hindi': 'hi', 'Hungarian': 'hu', 'Indonesian': 'id', 'Italian': 'it', 'Japanese': 'ja', 'Korean': 'ko', 'Latvian': 'lv', 'Macedonian': 'mk', 'Malay': 'ms', 'Norwegian': 'nb', 'Persian': 'fa', 'Polish': 'pl', 'Portuguese': 'pt', 'Romanian': 'ro', 'Russian': 'ru', 'Serbian': 'sr', 'Spanish': 'es', 'Swedish': 'sv', 'Turkish': 'tr', 'Ukrainian': 'uk'}
+# with open("list.txt") as f:
+#     r = f.read().split("\n")
+#     r = [list(filter(lambda x: x!="", l.split(" "))) for l in r]
+#     r = [l[0:2] for l in r]
+#     for l in r:
+#         lang_dict[l[0]] = l[1]
+#     print(lang_dict)
+
