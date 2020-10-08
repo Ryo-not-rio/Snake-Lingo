7e86c493f7275be1c322b8639c8319a64275bde7
Ryo-not-rio <ryo.not.rio.suzuki@gmail.com>
2020-10-08 18:49:57 +0100

    movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as py
 import settings
 import snake
+import time
 
 py.init()
 
@@ -8,20 +9,36 @@ display = py.display.set_mode((settings.DISPLAY_SIZE, settings.DISPLAY_SIZE))
 clock = py.time.Clock()
 
 gameExit = False
+prev_move = time.time()
 snake = snake.Snake()
 
 while not gameExit:
     for event in py.event.get():
         if event.type == py.QUIT:
             gameExit = True
+        if event.type == py.KEYDOWN:
+            if event.key in [py.K_LEFT, py.K_a]:
+                snake.change_directions('left')
+            if event.key in [py.K_RIGHT, py.K_d]:
+                snake.change_directions('right')
+            if event.key in [py.K_UP, py.K_w]:
+                snake.change_directions('up')
+            if event.key in [py.K_DOWN, py.K_s]:
+                snake.change_directions('down')
 
-    # Draw columns and rows
-    for i in range(settings.NUM_ROWS_COLUMNS):
-        py.draw.line(display, py.Color('white'), (i*settings.BLOCK_SIZE, settings.DISPLAY_SIZE), (i*settings.BLOCK_SIZE, 0))
-        py.draw.line(display, py.Color('white'), (0, i*settings.BLOCK_SIZE), (settings.DISPLAY_SIZE, i*settings.BLOCK_SIZE))
+    ### Drawing ###
+    if time.time() - prev_move > 1/settings.MOVES_PER_SECOND:
+        display.fill(py.Color('black'))
 
+        # Draw columns and rows
+        for i in range(settings.NUM_ROWS_COLUMNS):
+            py.draw.line(display, py.Color('white'), (i*settings.BLOCK_SIZE, settings.DISPLAY_SIZE), (i*settings.BLOCK_SIZE, 0))
+            py.draw.line(display, py.Color('white'), (0, i*settings.BLOCK_SIZE), (settings.DISPLAY_SIZE, i*settings.BLOCK_SIZE))
 
-    snake.draw(display)
+        snake.move()
+        snake.draw(display)
+        prev_move = time.time()
+
     py.display.update()
     clock.tick(30)
 
--- snake.py
+++ snake.py
@@ -12,6 +12,24 @@ class Snake:
 
     def move(self):
         self.position = list(np.array(self.position) + np.array(self.vel))
+        if self.position[0] < 0:
+            self.position[0] = settings.NUM_ROWS_COLUMNS-1
+        if self.position[0] >= settings.NUM_ROWS_COLUMNS:
+            self.position[0] = 0
+        if self.position[1] < 0:
+            self.position[1] = settings.NUM_ROWS_COLUMNS-1
+        if self.position[1] >= settings.NUM_ROWS_COLUMNS:
+            self.position[1] = 0
+
+
+    def change_directions(self, direction):
+        if direction is not None:
+            direction_str = ['up', 'down', 'left', 'right']
+            direction_arr = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+            new_vel = direction_arr[direction_str.index(direction)]
+            if (np.multiply(np.array(new_vel), np.array(self.vel)) == np.array((0, 0))).all():
+                self.vel = new_vel
+
 
     def draw(self, display):
         display.blit(self.head_surface, settings.grid_to_pos(self.position))
